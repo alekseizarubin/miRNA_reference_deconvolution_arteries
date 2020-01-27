7ac526d858ccf9ef8a3e3982f7107d1796c8209d
alekseizarubin <a.a.zarubin@gmail.com>
2020-01-27 03:05:10 +0000

    0.01

--- RSkript/deconvolution_reference.R
+++ RSkript/deconvolution_reference.R
@@ -7,8 +7,9 @@ colnames(fantom_o)<-Par2$name
 rownames(fantom_o)<-rownames(fantom_c)
 fantom_CAM <- CAM(as.data.frame(fantom_o), K = 2:10, thres.low = 0.10, thres.high = 0.95)
 
-tiff(filename ="MDL_reference.tiff",width = 6000,height = 3000,compression = "lzw",res = 400 )
+png(filename ="images/MDL_reference.jpg",width = 6000,height = 3000,res = 400 )
 plot(MDL(fantom_CAM), data.term = TRUE)
 dev.off()
 Aest <- Amat(fantom_CAM, 7)
 row.names(Aest)<-colnames(as.data.frame(fantom_o))
+write.table(Aest,file = "debCAM_7component.txt",sep = "\t",quote = T)
